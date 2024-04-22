a7e81e8c1f24db69e8492a1f5c9e315b1fb00fd3
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-04-22 14:56:58 -0500

    Add files via upload
    Changed variable names and added some if/else statements.

--- 100_Days_Day_3.py
+++ 100_Days_Day_3.py
@@ -7,9 +7,25 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.\n")
 
 # Scenarios
-bill_before_tip = input("You\'re at a cross road.  Where do you want to go?  Type \"left\" or \"right\".\n")
-people_splitting = input("You come to a lake.  There is an island in the middle of the lake.  Type \"wait\" to wait for a boat, or type \"swim\" to swim across.\n")
-tip_possibility = input("You arrive at the island unharmed.  There is a house with 3 doors.  One is red, one is yellow, and one is blue.  Type which color you choose.\n")
+cross_road = input("You\'re at a cross road.  Where do you want to go?  Type \"left\" or \"right\".\n")
+if cross_road = "left":
+    print("You choose left.")
+else:
+    print("You choose right.")
+
+island_lake = input("You come to a lake.  There is an island in the middle of the lake.  Type \"wait\" to wait for a boat, or type \"swim\" to swim across.\n")
+if island_lake = "wait":
+    print("You choose wait.")
+else:
+    print("You choose swim.")
+
+house_3_doors = input("You arrive at the island unharmed.  There is a house with 3 doors.  One is red, one is yellow, and one is blue.  Type which color you choose.\n")
+if house_3_doors = "red":
+    print("You choose red.")
+elif:
+    print("You chose yellow.")
+else:
+    print("You choose blue.")
 
 # Final Outcome
 print()
