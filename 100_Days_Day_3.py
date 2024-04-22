9b9f3c31fc24e4f78d5e7e2b7acaf3c7a4ef0c5c
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-04-22 13:40:28 -0500

    Add files via upload
    Beginning project...

---
+++ 100_Days_Day_3.py
@@ -0,0 +1,16 @@
+###################
+# Treasure Island #
+###################
+
+# Beginning of Game
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.\n")
+
+# Scenarios
+bill_before_tip = input("You\'re at a cross road.  Where do you want to go?  Type \"left\" or \"right\".\n")
+people_splitting = input("You come to a lake.  There is an island in the middle of the lake.  Type \"wait\" to wait for a boat, or type \"swim\" to swim across.\n")
+tip_possibility = input("You arrive at the island unharmed.  There is a house with 3 doors.  One is red, one is yellow, and one is blue.  Type which color you choose.\n")
+
+# Final Outcome
+print()
+print(f"GAME OVER")
